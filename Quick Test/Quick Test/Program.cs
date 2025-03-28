8f28a1c07355dd916ef9f63f8a2b7a9f96b374ea
Patrick Bachmann Vanting Jarshøj <paj003@edu.zealand.dk>
2025-03-28 09:35:49 +0100

    Done

--- Quick Test/Quick Test/Program.cs
+++ Quick Test/Quick Test/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Quick_Test
+﻿using Microsoft.VisualBasic;
+
+namespace Quick_Test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Code Challenge
+            //Bambus
+
         }
+    }
+    public class MysteryBox
+    {
+        private string _label;
+        private string _contains;
+        private bool _isExplosive1;
+        public string Label { get => _label; set => _label = value; }
+        public string Contains { get => _contains; set => _contains = value; }
+        public bool IsExplosive { get => _isExplosive1; set => _isExplosive1 = value; }
+
+        public MysteryBox(string label, string contains, bool isExplosive)
+        {
+            Label = label;
+            Contains = contains;
+            IsExplosive = isExplosive;
+
+        }
+        public override string ToString()
+        {
+            return $"Your mystery box has the Label {Label}. It contains {Contains}, and might be explosive: {IsExplosive}";
+        }
+
     }
 }
